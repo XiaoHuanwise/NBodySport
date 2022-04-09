af5302af818f26626c9143f0d02824cee2245794
XiaoHuanwise <1056366209@qq.com>
2022-04-09 22:03:52 +0800

    add

---
+++ n body sport/Numerical_Simulation/Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using N_body_sport;
+
+namespace Console {
+  class Program {
+    static void Main(string[] args) {
+      CelestialBody[] cbody = new CelestialBody[3];
+      for (int i = 0; i < cbody.Length; i++) {
+        cbody[i] = new CelestialBody();
+      }
+      double G = Initialization.RandomIni(cbody);
+      System.Console.WriteLine(G);
+    }
+  }
+}
